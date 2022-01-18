bd2e786f9a166361d0d51377f41c81b605793dee
Ronald Mosomi <ronaldmosomi@gmail.com>
2022-01-18 10:20:31 +0300

    vue clie project 1

---
+++ src/components/FriendsContacts.vue
@@ -0,0 +1,31 @@
+<template>
+<h2>{{friend.name}}</h2>
+<button @click="changeDetails"> {{detailsAreVisible? 'Hide':'show'}}  Details</button>
+<ul v-if="detailsAreVisible">
+    <li> <strong>Phone: </strong> {{friend.phone}} </li>
+    <li><strong>email: </strong> {{friend.email}} </li>
+</ul>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible:false,
+            friend:{
+                id:1,
+          name:'Ronald',
+          phone:'0725029795',
+          email:'ronidad@gmail.com'
+            }
+        };
+        
+    },
+    methods: {
+        changeDetails(){
+            this.detailsAreVisible=!this.detailsAreVisible;
+        }
+    }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import FriendContacts from './components/FriendsContacts.vue'
+
+
+const app = createApp(App);
+app.component('friend-contact', FriendContacts)
+
+
+app.mount('#app')
+// createApp(App).mount('#app')
